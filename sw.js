89a05952ed09cdfc2346f82d9632615219772ab9
swarooppatilx <swarooppatil850@gmail.com>
2024-11-08 18:53:03 +0530

    minor changes

--- sw.js
+++ sw.js
@@ -1,39 +1,47 @@
-const CACHE_NAME = `Portfolio`;
+const CACHE_NAME = 'portfolio';
+const CACHE_ASSETS = [
+  '/',
+  '/images',
+  '/css',
+  '/font',
+  '/js',
+  '/sass'
+];
 
-// Use the install event to pre-cache all initial resources.
+// Pre-cache assets during the install event
 self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      '/images',
-      '/css',
-      '/font',
-      '/js',
-      '/sass'
-    ]);
-  })());
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_ASSETS))
+  );
 });
 
+// Clean up old caches during the activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+// Network-first strategy for fetching assets
 self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
-
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-    }
-  })());
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // If the request succeeds, update the cache and return the response
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        // If the network request fails, try to get it from the cache
+        return caches.match(event.request);
+      })
+  );
 });
